2bebddb2a57bf626196de5f6bea85578acda489a
huhu <huhu@qq.com>
2025-10-18 17:39:29 +0800

    hot-fix第一次提交

--- src/main/java/com/example/gittext1/GitText1Application.java
+++ src/main/java/com/example/gittext1/GitText1Application.java
@@ -11,6 +11,8 @@ public class GitText1Application {
         System.out.println(("----- selectAll method test ------"));
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hot-fix 分支");
+
     }
 
 }
