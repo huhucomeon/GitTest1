2d07e4a616a63a6864640d9e72bc1c10f14f79c5
huhu <huhu@qq.com>
2025-10-18 17:10:01 +0800

    git学习第二次提交

--- src/main/java/com/example/gittext1/GitText1Application.java
+++ src/main/java/com/example/gittext1/GitText1Application.java
@@ -9,6 +9,7 @@ public class GitText1Application {
     public static void main(String[] args) {
         SpringApplication.run(GitText1Application.class, args);
         System.out.println(("----- selectAll method test ------"));
+        System.out.println("hello git1");
     }
 
 }
