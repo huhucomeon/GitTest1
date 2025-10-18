8b5d45f3cfd370c87ac33eb28058b04e8c735999
huhu <huhu@qq.com>
2025-10-18 17:04:32 +0800

    git学习第一次提交

---
+++ src/main/java/com/example/gittext1/GitText1Application.java
@@ -0,0 +1,14 @@
+package com.example.gittext1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitText1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitText1Application.class, args);
+        System.out.println(("----- selectAll method test ------"));
+    }
+
+}
---
+++ src/test/java/com/example/gittext1/GitText1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittext1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitText1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
